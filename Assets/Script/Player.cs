895da116710b95dda8d4585c142a4fc8452f7098
Hiroshiki123 <79053749+Hiroshiki123@users.noreply.github.com>
2024-10-28 11:57:05 -0300

    aaaa

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,10 +8,11 @@ public class Player : MonoBehaviour
     public float speed;
     public float limiteTop;
     public float limiteBottom;
+    private float color;
     // Start is called before the first frame update
     void Start()
     {
-        
+        color = Random.Range(1, 4);
     }
 
     // Update is called once per frame
@@ -19,9 +20,42 @@ public class Player : MonoBehaviour
     {
         limiteTop = transform.position.y + 16;
         limiteBottom = transform.position.y - 16;
+        if (color == 1)
+        {
+
+        }else if (color == 2)
+        {
+
+        }else if (color == 3)
+        {
+
+        }else if (color == 4)
+        {
+
+        }
         if (Input.GetButton("Fire1"))
         {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "azul")
+        {
+
+        }
+        if (collision.gameObject.tag == "amarelo")
+        {
+
+        }
+        if (collision.gameObject.tag == "rosa")
+        {
+
+        }
+        if (collision.gameObject.tag == "roxo")
+        {
+
         }
     }
 }
