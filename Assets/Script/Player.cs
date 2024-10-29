7beedc8cb07beec419c83bacfcbe6e438724c105
Hiroshiki123 <79053749+Hiroshiki123@users.noreply.github.com>
2024-10-29 09:42:05 -0300

    awsf

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,11 +10,13 @@ public class Player : MonoBehaviour
     public float limiteBottom;
     private float color;
     private SpriteRenderer spritecor;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         color = Random.Range(1, 4);
         spritecor = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,10 @@ public class Player : MonoBehaviour
     {
         limiteTop = transform.position.y + 16;
         limiteBottom = transform.position.y - 16;
+        if (Input.GetButton("Fire1"))
+        {
+            rb.velocity = Vector2.up*speed;
+        }
         if (color == 1)
         {
             spritecor.color = new Color(0,255,255,255);
@@ -37,29 +43,38 @@ public class Player : MonoBehaviour
         {
             spritecor.color = new Color(125, 0, 255, 255);
         }
-        if (Input.GetButton("Fire1"))
-        {
-            transform.Translate(Vector2.up * speed * Time.deltaTime);
-        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "ciano")
         {
-
+            if (color != 1)
+            {
+                Destroy(gameObject);
+            }
         }
         if (collision.gameObject.tag == "amarelo")
         {
-
+            if (color != 2)
+            {
+                Destroy(gameObject);
+            }
         }
         if (collision.gameObject.tag == "rosa")
         {
-
+            if (color != 3)
+            {
+                Destroy(gameObject);
+            }
         }
         if (collision.gameObject.tag == "roxo")
         {
-
+            if (color != 4)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
